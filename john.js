72bcf120685a6b78a423f934eeb0e0f4a47db21e
Syeda-Rohab <syedarohab8@gmail.com>
2024-07-16 06:16:17 -0700

    Add files via upload

---
+++ john.js
@@ -0,0 +1,5 @@
+"use strict";
+let weightinkg = 1500000;
+let heightinmeter = 1500000;
+let bmi = weightinkg / (heightinmeter * heightinmeter);
+console.log(`your bmi is ${bmi}`);
---
+++ john.ts
@@ -0,0 +1,4 @@
+let weightinkg = 1500000;
+let heightinmeter = 1500000;
+let bmi = weightinkg / ( heightinmeter * heightinmeter );
+console.log(`your bmi is ${bmi}`);
